0f9fc0aaed26ea3c272d499430dabdf89142fafa
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-02-08 20:51:10 +0300

    Completed Project

--- js/main.js
+++ js/main.js
@@ -1,44 +1,39 @@
 let todos = [
  
 ];
-
+if(localStorage.getItem("todos") !== null){
+    todos =  JSON.parse(localStorage.getItem("todos"));
+}
 let listDOM = document.querySelector('#list');
 function getTodos(){
-    todos.forEach(item =>{
-        let liElement = document.createElement('li');
-        liElement.innerHTML = `
-                <div class="row" >
+    listDOM.innerHTML = '';
+    todos.forEach(item =>{        
+        let liElement = `
+                <div class="row"  id = ${item.id}>
                     <div class="col-9">
-                            ${item.title}
+                            ${item.taskName}
                     </div>
                     <div class="col-3 float-right">
-                        <button onclick="removeElement(${item.id})" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i></button>
+                    <button onclick="completeTodo('${item.id}')" class="btn btn-outline-primary btn-sm"><i class="fa fa-check"></i></button>
+                        <button onclick="removeElement('${item.id}')" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i></button>
                     </div>
                 </div>
         `;
-        listDOM.appendChild(liElement) ;
+        listDOM.insertAdjacentHTML("beforeend", liElement);
     });
 }
 
 function newElement(){
     let task = document.querySelector('#task');
     if(task.value){
-        let todo = {id:todos.length+1,title:task.value}
-       todos.push(todo);
-       localStorage.setItem('todos',JSON.stringify(todos));
-       let liElement = document.createElement('li');
-       liElement.innerHTML = `
-               <div class="row" >
-                   <div class="col-9">
-                           ${todo.title}
-                   </div>
-                   <div class="col-3 float-right">
-                       <button onclick="removeElement(${todo.id})" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i></button>
-                   </div>
-               </div>
-       `;
-       listDOM.appendChild(liElement) ;
-       
+        todo = {
+            id: todos.length,
+            taskName: task.value
+        }
+        todos.push(todo);
+        getTodos();
+        task.value = '';
+        localStorage.setItem('todos', JSON.stringify(todos));
     }else {
         console.log("Error");
     }
@@ -46,5 +41,29 @@ function newElement(){
 
 function removeElement(id){
     
-    getTodos(); 
+    let deletedID;
+    for (let index in todos) {
+        if(todos[index].id == id) {
+            deletedID = index;
+        }
+    }
+    todos.splice(deletedID, 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    getTodos();
+}
+
+
+function completeTodo(id) {
+    let checkedID;
+    for(let item in todos){
+        if(todos[item].id == id){
+            checkedID = item;
+        }
+    }    
+    let classList = document.getElementById(`${checkedID}`).classList;    
+    if (classList.contains("checked")){
+        classList.remove("checked")
+    }else{
+        classList.add("checked");
+    }
 }
